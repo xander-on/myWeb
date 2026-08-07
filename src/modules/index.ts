58f6407703692eee5fb65bb3b62b016e5044d69a
Alexander Moreno <alexandermoreno.dev@gmail.com>
2026-08-06 21:51:43 -0500

    last commit

--- src/modules/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./admin";
-export * from "./auth";
-export * from "./cv";
-export * from "./projects";
-export * from "./certificates";
-export * from "./web";
-export * from "./shared";
