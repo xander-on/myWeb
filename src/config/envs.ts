cb7e1acaccc2cee03feb68bf4d74c2c180203bea
Alexander Moreno <alexandermoreno.dev@gmail.com>
2026-09-04 12:12:42 -0500

    add env backend url

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,5 +1,5 @@
 export const envs = {
   strapi        : import.meta.env.VITE_STRAPI_URL,
   strapiGraphql : import.meta.env.VITE_STRAPI_GRAPHQL,
-  adminApi      : import.meta.env.VITE_ADMIN_API_URL,
+  adminApi      : import.meta.env.VITE_BACKEND_URL,
 }
