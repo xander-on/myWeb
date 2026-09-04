28584ea7594482dd8ff53ba44eb3b65886cc950a
Alexander Moreno <alexandermoreno.dev@gmail.com>
2026-09-04 12:34:48 -0500

    fix(api): update backend API baseURL to use backendUrl from envs

--- src/config/api/backendApi.ts
+++ src/config/api/backendApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { envs } from "@/config/envs";
 
 export const backendApi = axios.create({
-  baseURL: envs.adminApi,
+  baseURL: `${envs.backendUrl}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
